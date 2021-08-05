afbf1af588b840b7f929308e9d1be626347bae62
shuai.yang001 <shuai.yang001@ke.com>
2021-08-05 23:31:03 +0800

    add draw lottery

--- core/juejin.py
+++ core/juejin.py
@@ -106,6 +106,42 @@ class Juejin(object):
         return result
 
 
+def lottery():
+
+    # session id 自行设置
+    session_id = ""
+
+    cookie = requests.cookies.create_cookie(
+        domain=".juejin.cn",
+        name="sessionid",
+        value=session_id
+    )
+    juejin = Juejin(cookie_obj=cookie)
+
+    gift = {}
+    num = 1
+
+    while True:
+        result = juejin.draw_lottery()
+        if result.get("err_no") == 0:
+            lottery_name = result.get("data", {}).get("lottery_name")
+            if gift.get(lottery_name):
+                gift[lottery_name] += 1
+            else:
+                gift[lottery_name] = 1
+            print(f"第{num}次-抽奖结果为：{lottery_name}")
+            num += 1
+        else:
+            print(result.get("err_msg"))
+            break
+
+    print(f"总抽奖次数为：{num}")
+
+    print("最终抽奖结果为：")
+    for k, v in gift.items():
+        print(f"礼物：{k} ---- 个数：{v}")
+
+
 def update_and_republish():
 
     # 定义活动时间
@@ -199,4 +235,5 @@ def update_and_republish():
 
 
 if __name__ == '__main__':
-    update_and_republish()
+    # update_and_republish()
+    lottery()
