fd0426909b343774425573203258955ac842653b
shuai <785363268@qq.com>
2022-07-17 17:08:51 +0800

    Create juejin_driver.py

--- core/juejin_driver.py
+++ core/juejin_driver.py
@@ -13,7 +13,7 @@ from .config import *
 class JuejinDriver(object):
 
     # 掘金首页
-    juejin_home = "https://juejin.cn/"
+    juejin_home = "https://juejin.cn/post/7101932322118713380"
 
     # 掘金签到页面
     juejin_sign = "https://juejin.cn/user/center/signin"
